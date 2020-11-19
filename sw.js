3a99605e75e679bc268eed5b14167deba9907647
Sandra Gil <scgmendozaprivate16@gmail.com>
2020-11-18 22:18:09 -0600

    TypeScript challenge

--- sw.js
+++ sw.js
@@ -19,16 +19,16 @@ self.addEventListener('fetch', event => {
 });
 
 async function precache() {
-    const cache = await caches.open(VERSON);
+    const cache = await caches.open(VERSION);
     return cache.addAll([
-      '/',
-      '/index.html',
-      '/assets/index.js',
-      '/assets/MediaPlayer.js',
-      '/assets/plugins/AutoPlay.js',
-      '/assets/plugins/AutoPause.js',
-      '/assets/index.css',
-      '/assets/CatVideo.mp4',
+   //   '/',
+  //    '/index.html',
+   //   '/assets/index.js',
+   //   '/assets/MediaPlayer.js',
+   //   '/assets/plugins/AutoPlay.js',
+    //  '/assets/plugins/AutoPause.js',
+   ///  '/assets/index.css',
+   //   '/assets/CatVideo.mp4',
     ]);
 }
 
